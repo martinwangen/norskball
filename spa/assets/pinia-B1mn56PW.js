a555e3288ed3451da7e545554dc47cd2464caf35
martinwangen <martinwangen@users.noreply.github.com>
2025-03-27 08:49:31 +0000

    deploy: ae712b1ee1590881c760de917d54f72f238edfda

---
+++ spa/assets/pinia-B1mn56PW.js
@@ -0,0 +1,1 @@
+import{j as o,k as t}from"./index-Cm6bZCtb.js";const e=o(({app:a})=>{const i=t();a.use(i)});export{e as default};
---
+++ spa/assets/use-quasar-BjZ55HgA.js
@@ -0,0 +1,1 @@
+import{a,ap as r}from"./index-Cm6bZCtb.js";function u(){return a(r)}export{u};
